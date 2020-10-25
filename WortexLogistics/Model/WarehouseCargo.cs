147f0b6b280c2e81a5a48fd2d8b3297992a7a941
Chesternal <chesternal@gmail.com>
2020-10-25 13:21:56 +0200

    Update WarehouseCargo.cs

--- WortexLogistics/Model/WarehouseCargo.cs
+++ WortexLogistics/Model/WarehouseCargo.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WortexLogistics.Model
+namespace Testinqa.Models
 {
-    public class WarehouseCargo
+    public partial class WarehouseCargo
     {
+        [Key]
+        [Column(TypeName = "int")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        [Display(Name = "Cargo Name")]
+        public string WCargoName { get; set; }
+
+        [Column(TypeName = "smallint")]
+        [Display(Name = "Cargo Count")]
+        public string WCargoCount { get; set; }
+
+        [Column(TypeName = "decimal(4,2)")]
+        [Display(Name = "Cargo Weight")]
+        public string WCargoWeight { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        [Display(Name = "Cargo Name")]
+        public string WCargoOrigin { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        [Display(Name = "Cargo Name")]
+        public string WCargoDestination { get; set; }
+
+
+        public WarehouseCargo GetAnimal()
+        {
+            return new WarehouseCargo
+            {
+                WCargoName = WCargoName,
+                WCargoCount = WCargoCount,
+                WCargoWeight = WCargoWeight,
+                WCargoOrigin = WCargoOrigin,
+                WCargoDestination = WCargoDestination
+            };
+        }
     }
 }
