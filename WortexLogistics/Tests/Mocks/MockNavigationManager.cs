2757b663d86f05cdfd5eadeb0c17de90d668a94e
Lauri Leiten <starrimus7@gmail.com>
2020-10-25 23:11:13 +0200

    WLC_F02 test not compiling

---
+++ WortexLogistics/Tests/Mocks/MockNavigationManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WortexLogistics.Tests.Mocks
+{
+    public class MockNavigationManager : NavigationManager
+    {
+        public MockNavigationManager()
+        {
+            this.Initialize("https://test.com/", "https://test.com/testlink/");
+        }
+
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            this.Uri = uri;
+        }
+    }
+}
--- WortexLogistics/Tests/WLC_F02.cs
+++ WortexLogistics/Tests/WLC_F02.cs
@@ -5,30 +5,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Bunit;
 using Bunit.TestDoubles.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
 using WortexLogistics.Areas.Identity.Pages.Account;
 using WortexLogistics.Shared;
+using WortexLogistics.Tests.Mocks;
 using Xunit;
 using Xunit.Abstractions;
 using Index = WortexLogistics.Pages.Index;
-
+using TruckIndex = WortexLogistics.Pages.TruckCargos.IndexModel;
+    
 namespace WortexLogistics.Tests
 {
     public class WLC_F02
     {
         [Fact]
-        public void AuthorizedUserSeeRole()
+        public void TransportWorkerCanSeeTruckInfoNavigationButton()
         {
             // Arrange
             using var ctx = new TestContext();
+            ctx.Services.AddSingleton<NavigationManager>(new MockNavigationManager());
+
             var authContext = ctx.Services.AddTestAuthorization();
             authContext.SetAuthorized("TEST USER");
-            authContext.SetRoles("manager");
+            authContext.SetRoles("transportworker");
 
             //// Act
-            var cut = ctx.RenderComponent<Index>();
+            var cut = ctx.RenderComponent<NavMenu>();
 
             //// Assert
-            Assert.True(cut.Markup.Contains("Role: manager"));
+            Assert.Contains("Truck", cut.Markup);
         }
     }
 }
